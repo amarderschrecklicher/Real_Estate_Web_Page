68a341ce43a926e06319555fd0aa08bc455e8cd4
amarderschrecklicher <115952128+amarderschrecklicher@users.noreply.github.com>
2023-12-21 21:38:49 +0100

    login logout

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ const fs = require('fs');
 const bcrypt = require('bcrypt');
 const path = require('path');
 const bodyParser = require('body-parser')
+const session = require("express-session")
 const app = express();
 app.use(express.static('public'));
 app.listen(3000);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(session({
+    secret: 'code',
+    resave: true,
+    saveUninitialized: true,
+ }));
 
 const filePath = path.join(__dirname, 'data', 'korisnici.json');
 
@@ -20,17 +28,17 @@ app.post('/login',function(req,res){
           return;
         }   
         try {
-          bcrypt.hash(password, 10, function(err, hash) {
+
             const korisnici = JSON.parse(data);
-            var a = korisnici.find(korisnik => korisnik.username == username && korisnik.password == hash)
+            var a = korisnici.find(korisnik => korisnik.username == username && korisnik.password == password )
             if(a){
-                res.status(200).json({poruka:"Uspješna prijava"})
                 req.session.username = username;
+                res.status(200).json({poruka:"Uspješna prijava"})     
             }
             else {
-                res.status(401).json({poruka:"Neuspješna prijava"})
+                res.status(401).json({greska:"Neuspješna prijava"})
             }
-            });  
+
 
         } catch (error) {
           console.error('Error parsing JSON data:', error);
@@ -40,5 +48,12 @@ app.post('/login',function(req,res){
 
 app.post('/logout',function(req,res){
     
-   
+      if (req.session.username)
+      {
+        req.session.username = null
+        res.status(200).json({poruka:"Uspješno ste se odjavili"})
+      }
+      else{
+        res.status(401).json({greska:"Neautorizovan pristup"})
+      }
 });
